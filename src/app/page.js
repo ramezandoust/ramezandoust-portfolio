a2cb92f0ed92fd36a44e626f61dbead91b25ce95
ramezandoust <abolfazl.ramezandoust@gmail.com>
2023-12-11 01:53:40 +0330

    design main page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,16 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>ابوالفضل رمضان دوست</h1>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <Image className={styles.mypic} src="/ramezandoust.jpeg" alt="ramezandoust" width={400} height={400} />
+        </div>
+
+        <div className={styles.col}>
+          <h1>ابوالفضل رمضان دوست</h1>
+          <p>طراح وب سایت و توسعه دهنده فرانت اند</p>
+        </div>
+      </div>
     </main>
   );
 }
